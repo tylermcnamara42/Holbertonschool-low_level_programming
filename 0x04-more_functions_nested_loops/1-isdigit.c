c9687f216318f7bd077abe1b7bed3045cc1cb033
root <tylermcnamara42@gmail.com>
2022-09-28 10:33:45 -0700

    My first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
+/**
+ * _isdigit - Checks for a digit (0-9).
+ * @c: The number to be checked.
+ *
+ * Return: 1 if the number is a digit, 0 otherwise.
+ */
 int _isdigit(int x)
 {
 	if (x >= '0' && x <= '9')
-
-	return (1);
+		return (1);
 
 	else
-
-	return (0)
+		return (0)
 }
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,8 +12,27 @@ int print_last_digit(int);
 void jack_baurer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-void reset_to_98(int *n);
+void print_to_98(int *n);
+int _isupper(int c);
+void swap_int(int *a, int *b);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void)
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif // main_h_
 
